cc3eaa3cf56b9a57f9650104283d3220fece0eab
lembathomas <lembathomas@gmail.comgit config --global user.name lembathomas>
2022-09-22 13:28:55 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,10 +4,14 @@
 * _islower - Short description, single line
 * Return: Always 0.
 */
-
 int _islower(int c)
-
 {
-
-return (c >= 'a' && c <= 'z');
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
