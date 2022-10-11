edd9a9c55ef50686091da43f7e6d31eb44ce5430
lembathomas <lembathomas@gmail.comgit config --global user.name lembathomas>
2022-10-11 12:12:38 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,21 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 /**
-* init_dog - initialize a variable of type struct dog
-* @d: array 
+* struct dog - Struct dog
 * @name: name
 * @age: age
-* @owner: owner
+* @owner:  owner
 * Description: This struct is for dog
 */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-if (d == NULL)
-return;
-else
-{
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
-}
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /*DOG_H*/
