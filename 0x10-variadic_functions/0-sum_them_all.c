54dcf549e0c4f579fb05c194789dc7ca4a57f236
lembathomas <lembathomas@gmail.comgit config --global user.name lembathomas>
2022-11-03 03:07:22 -0700

    0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,16 +5,21 @@
 * @n: the number of parameters passed
 * Return: Always 0
 */
+
 int sum_them_all(const unsigned int n, ...)
+
 {
-int sum;
-unsigned int i;
-va_list ap;
-va_start(ap, n);
-if (n == 0)
-return (0);
-or (i = 0; i < n; i++)
-sum += va_arg(ap, int);
-va_end(ap);
-return (sum);
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
+
+	return (sum);
 }
