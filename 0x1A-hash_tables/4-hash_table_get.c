2465b4f1b9952800eac3bc787eaff15edfd3f846
lembathomas <lembathomas@gmail.comgit config --global user.name lembathomas>
2023-01-12 13:26:03 -0800

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,25 +1,25 @@
 #include "hash_tables.h"
 /**
-* hash_table_get - retrieves a value associated with a key
-* @ht: table to retrieve value from
-* @key: key to find value
-* Return: value associated with key, or NULL if key cannot be found
+* hash_table_get - Retrieve the value associated with
+*    a key in a hash table.
+* @ht: A pointer to the hash table.
+* @key: The key to get the value of.
+* Return: If the key cannot be matched - NULL.
+*  Otherwise - the value associated with key in ht.
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *tmp;
+	hash_node_t *node;
 
-	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-	key == NULL || strlen(key) == 0)
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
 	return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-	while (tmp != NULL)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		return (tmp->value);
-		tmp = tmp->next;
-	}
+	if (index >= ht->size)
 	return (NULL);
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+	node = node->next;
+	return ((node == NULL) ? NULL : node->value);
 }
